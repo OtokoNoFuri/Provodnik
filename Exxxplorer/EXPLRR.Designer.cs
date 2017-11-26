2951ffb4fa94bf35dd7574413a95fa47bf164c79
Orendi JJ <Zer0Meisterr@Gmail.com>
2017-11-26 20:44:17 +0300

    Добавьте файлы проекта.

---
+++ Exxxplorer/EXPLRR.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Exxxplorer
+{
+    partial class Explrr
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Discsinfo = new System.Windows.Forms.DataGridView();
+            this.Drivers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DirInfo = new System.Windows.Forms.DataGridView();
+            this.Looking_for_what = new System.Windows.Forms.TextBox();
+            this.goBack = new System.Windows.Forms.Button();
+            this.tip1 = new System.Windows.Forms.Label();
+            this.type_search = new System.Windows.Forms.TextBox();
+            this.tip = new System.Windows.Forms.Label();
+            this.Naaaame = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sizeee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Discsinfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DirInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Discsinfo
+            // 
+            this.Discsinfo.AllowUserToAddRows = false;
+            this.Discsinfo.AllowUserToDeleteRows = false;
+            this.Discsinfo.AllowUserToResizeColumns = false;
+            this.Discsinfo.AllowUserToResizeRows = false;
+            this.Discsinfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Discsinfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Drivers});
+            this.Discsinfo.Location = new System.Drawing.Point(409, 12);
+            this.Discsinfo.MultiSelect = false;
+            this.Discsinfo.Name = "Discsinfo";
+            this.Discsinfo.ReadOnly = true;
+            this.Discsinfo.RowHeadersVisible = false;
+            this.Discsinfo.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.Discsinfo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Discsinfo.Size = new System.Drawing.Size(105, 149);
+            this.Discsinfo.TabIndex = 0;
+            // 
+            // Drivers
+            // 
+            this.Drivers.HeaderText = "Диски";
+            this.Drivers.Name = "Drivers";
+            this.Drivers.ReadOnly = true;
+            // 
+            // DirInfo
+            // 
+            this.DirInfo.AllowUserToAddRows = false;
+            this.DirInfo.AllowUserToDeleteRows = false;
+            this.DirInfo.AllowUserToResizeColumns = false;
+            this.DirInfo.AllowUserToResizeRows = false;
+            this.DirInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.DirInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Naaaame,
+            this.Type,
+            this.Sizeee});
+            this.DirInfo.Location = new System.Drawing.Point(2, 198);
+            this.DirInfo.MultiSelect = false;
+            this.DirInfo.Name = "DirInfo";
+            this.DirInfo.ReadOnly = true;
+            this.DirInfo.RowHeadersVisible = false;
+            this.DirInfo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DirInfo.Size = new System.Drawing.Size(512, 309);
+            this.DirInfo.TabIndex = 1;
+            this.DirInfo.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.Dir_Info_2Click);
+            // 
+            // Looking_for_what
+            // 
+            this.Looking_for_what.Location = new System.Drawing.Point(2, 38);
+            this.Looking_for_what.Name = "Looking_for_what";
+            this.Looking_for_what.Size = new System.Drawing.Size(160, 20);
+            this.Looking_for_what.TabIndex = 2;
+            this.Looking_for_what.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Looking_for_what_KeyUp);
+            // 
+            // goBack
+            // 
+            this.goBack.Enabled = false;
+            this.goBack.Location = new System.Drawing.Point(330, 108);
+            this.goBack.Name = "goBack";
+            this.goBack.Size = new System.Drawing.Size(73, 53);
+            this.goBack.TabIndex = 3;
+            this.goBack.Text = "Назад";
+            this.goBack.UseVisualStyleBackColor = true;
+            this.goBack.Click += new System.EventHandler(this.Backward_Click);
+            // 
+            // tip1
+            // 
+            this.tip1.Location = new System.Drawing.Point(281, 9);
+            this.tip1.Name = "tip1";
+            this.tip1.Size = new System.Drawing.Size(122, 23);
+            this.tip1.TabIndex = 7;
+            this.tip1.Text = "поиск по расширению";
+            // 
+            // type_search
+            // 
+            this.type_search.Location = new System.Drawing.Point(330, 38);
+            this.type_search.Name = "type_search";
+            this.type_search.Size = new System.Drawing.Size(73, 20);
+            this.type_search.TabIndex = 8;
+            this.type_search.KeyUp += new System.Windows.Forms.KeyEventHandler(this.EXTNSION_SEARCH_KeyUp);
+            // 
+            // tip
+            // 
+            this.tip.Location = new System.Drawing.Point(-1, 12);
+            this.tip.Name = "tip";
+            this.tip.Size = new System.Drawing.Size(199, 23);
+            this.tip.TabIndex = 9;
+            this.tip.Text = "Press print screen find what u need";
+            // 
+            // Naaaame
+            // 
+            this.Naaaame.HeaderText = "Название";
+            this.Naaaame.Name = "Naaaame";
+            this.Naaaame.ReadOnly = true;
+            this.Naaaame.Width = 250;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Вид";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            this.Type.Width = 120;
+            // 
+            // Sizeee
+            // 
+            this.Sizeee.HeaderText = "Размер";
+            this.Sizeee.Name = "Sizeee";
+            this.Sizeee.ReadOnly = true;
+            this.Sizeee.Width = 120;
+            // 
+            // Explrr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 519);
+            this.Controls.Add(this.tip);
+            this.Controls.Add(this.type_search);
+            this.Controls.Add(this.tip1);
+            this.Controls.Add(this.goBack);
+            this.Controls.Add(this.Looking_for_what);
+            this.Controls.Add(this.DirInfo);
+            this.Controls.Add(this.Discsinfo);
+            this.Name = "Explrr";
+            this.Text = "Проводник ";
+            this.Load += new System.EventHandler(this.Explrr_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Discsinfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DirInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Discsinfo;
+        private System.Windows.Forms.DataGridView DirInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Drivers;
+        private System.Windows.Forms.TextBox Looking_for_what;
+        private System.Windows.Forms.Button goBack;
+        private System.Windows.Forms.Label tip1;
+        private System.Windows.Forms.TextBox type_search;
+        private System.Windows.Forms.Label tip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Naaaame;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sizeee;
+    }
+}
+
---
+++ Exxxplorer/EXPLRR.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exxxplorer
+{
+    public partial class Explrr : Form
+    {
+        public Explrr()
+        {
+            InitializeComponent();
+        }
+        Stack<string> DiscsStack = new Stack<string>();
+        private void Explrr_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] drivers = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drivers)
+                Discsinfo.Rows.Add(drive);
+            Discsinfo.ClearSelection();
+            Discsinfo.SelectionChanged += new System.EventHandler(this.DiscsInfo_SelChan);
+        }
+        private void RefreshInfo(string Directry)
+        {
+            if (DiscsStack.Count <= 1) goBack.Enabled = false;
+            else goBack.Enabled = true;
+            try { Directory.SetCurrentDirectory(Directry); }
+            catch
+            {
+                Looking_for_what.Text = DiscsStack.Pop();
+                MessageBox.Show("N/A");
+                return;
+            }
+            DirInfo.Rows.Clear();
+            var directories = Directory.GetDirectories(Directory.GetCurrentDirectory());
+            DirectoryInfo dir;
+       
+            for( int i = 0; i< directories.Length; i++)
+            {
+                int n = DirInfo.Rows.Add();
+                DirInfo.Rows[n].Cells[0].Value = NameFileOrDir(directories[i]);
+                DirInfo.Rows[n].Cells[1].Value = "Папка с файлами";
+                dir = new DirectoryInfo(directories[i]);
+                try { DirInfo.Rows[n].Cells[2].Value = DirSize(dir); }
+                catch { DirInfo.Rows[n].Cells[2].Value = "N/A"; continue; }
+                DirInfo.Rows[n].Cells[2].Value = Math.Round(double.Parse(DirInfo.Rows[n].Cells[2].Value.ToString()) / 1048576, 2);
+                DirInfo.Rows[n].Cells[2].Value += " мб";
+            }
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory());
+         
+            for (int i = 0; i < files.Length; i++) 
+            {
+                int n = DirInfo.Rows.Add();
+                FileInfo file = new FileInfo(NameFileOrDir(files[i]));
+                DirInfo.Rows[n].Cells[0].Value = file.Name;
+                DirInfo.Rows[n].Cells[1].Value = typename(file.Extension);
+                DirInfo.Rows[n].Cells[2].Value = Math.Round((double)file.Length / 1048576, 2);
+            }
+        }
+        private void DiscsInfo_SelChan(object sender, EventArgs e)
+        {
+            DiscsStack.Clear();
+            DiscsStack.Push(Discsinfo.CurrentRow.Cells[0].Value.ToString());
+            Looking_for_what.Text = Discsinfo.CurrentRow.Cells[0].Value.ToString();
+            RefreshInfo(Looking_for_what.Text);
+        }
+        public long DirSize(DirectoryInfo dir)
+        {
+            try
+            {
+                long Size = 0;
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo fi in files)
+                {
+                    Size += fi.Length;
+                }
+                DirectoryInfo[] dis = dir.GetDirectories();
+                foreach (DirectoryInfo dInf in dis)
+                {
+                    Size += DirSize(dInf);
+                }
+                return Size;
+            }
+            catch { throw new Exception(); }
+        }
+        private static string NameFileOrDir(string str)
+        {
+            string result = null;
+            int i = str.Length - 1;
+            while (str[i--] != (char)92) ;
+            i += 2;
+            for (int q = i; q < str.Length; q++)
+                result += str[q];
+            return result;
+        }
+        private string typename(string str)
+        {
+            switch (str)
+            {
+                case ".docx":
+                    return "Документ Microsoft Word";
+                case ".txt":
+                    return "Текстовый документ";
+                case ".png":
+                case ".jpg":
+                case ".ico":
+                case ".gif":
+                case ".tif":
+                    return "Картиночка";
+                default:
+                    return "Extension";
+            }
+        }
+
+        private void Backward_Click(object sender, EventArgs e)
+        {
+            Looking_for_what.Text = DiscsStack.Pop();
+            RefreshInfo(Looking_for_what.Text);
+        }
+
+        private void Dir_Info_2Click(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (DirInfo.CurrentRow.Cells[1].Value.ToString() != "Папка с файлами")
+            {
+                System.Diagnostics.Process.Start(Looking_for_what.Text + DirInfo.CurrentRow.Cells[0].Value.ToString());
+                return;
+            }
+            DiscsStack.Push(Looking_for_what.Text);
+            Looking_for_what.Text += DirInfo.CurrentRow.Cells[0].Value.ToString() + ((char)92).ToString();
+            RefreshInfo(Looking_for_what.Text);
+        }
+
+        private void Looking_for_what_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Keys.PrintScreen != e.KeyCode) return;
+            if (!Directory.Exists(Looking_for_what.Text))
+            {
+                MessageBox.Show("Твоё путь - ложь");
+                return;
+            }
+            RefreshInfo(Looking_for_what.Text);
+        }
+
+        private void EXTNSION_SEARCH_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Keys.PrintScreen != e.KeyCode || DirInfo.RowCount == 0) return;
+            for(int i = 0; i < DirInfo.RowCount; i++)
+            {
+                FileInfo file = new FileInfo(NameFileOrDir(Looking_for_what.Text + DirInfo.Rows[i].Cells[0].Value.ToString()));
+                if (file.Extension != type_search.Text) DirInfo.Rows[i].Visible = false;
+            }
+        }
+    }
+}
